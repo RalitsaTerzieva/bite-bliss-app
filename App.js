587f96c6b132861624cfa7fd105d5628f2d7d066
Ralica Popova <ralicaterzieva90@gmail.com>
2024-07-07 16:05:36 +0300

    Add React Navigation

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StyleSheet } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <CategoryScreen/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

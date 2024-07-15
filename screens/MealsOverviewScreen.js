6d201a94db701e193db6c1f8f02304668dd64c35
Ralica Popova <ralicaterzieva90@gmail.com>
2024-07-15 16:18:19 +0300

    Use the MealItem component

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
     const catId = route.params.categoryId;
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
     return (
     <View style={styles.container}>
-        <Text>Meals Overview Screen - {catId}</Text>
+        <FlatList 
+        data={displayedMeals} 
+        keyExtractor={(item) => item.id} 
+        renderItem={renderMealItem}
+        />
     </View>
     )
 }

ec23010e40ff0f57376fa9d41df00081f1a305e1
Ralica Popova <ralicaterzieva90@gmail.com>
2024-07-16 16:58:08 +0300

    Add MealDetails component logic

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,6 @@
 import { View, Text, Pressable, Image, StyleSheet, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import MealDetails from './MealDetails';
 
 function MealItem({ id, title, imageUrl, duration, complexity, affordability }) {
     const navigation = useNavigation();
@@ -10,7 +11,6 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability }) 
         });
     }
 
-    
 
     return <View style={styles.mealItem}>
         <Pressable 
@@ -26,11 +26,7 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability }) 
                     />
                     <Text style={styles.title}>{title}</Text>
                 </View>
-                <View style={styles.details}>
-                    <Text style={styles.detailItem}>{duration} m</Text>
-                    <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                    <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-                </View>
+               <MealDetails duration={duration} complexity={complexity} affordability={affordability}/>
             </View>
         </Pressable>
     </View>
@@ -67,15 +63,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         margin: 8
-    }, 
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8
-    },
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12
     }
 });

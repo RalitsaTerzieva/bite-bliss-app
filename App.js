fbdbf3ba08b4b2677dd4eabcb3488300497ace59
Ralica Popova <ralicaterzieva90@gmail.com>
2024-07-16 19:52:40 +0300

    Add Header button component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, StatusBar } from 'react-native';
+import { StyleSheet, StatusBar, Button } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -35,7 +35,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
+  
 });
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,27 @@
-import { View, Image, Text, StyleSheet, ScrollView } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { View, Image, Text, StyleSheet, ScrollView, Button } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
     const mealId = route.params.mealId;
-    const selectedMeal = MEALS.find((meal) => meal.id === mealId )
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId );
+
+    function headerButtonPressHandler() {
+        console.log('Pressed!')
+    }
+
+    useLayoutEffect(() => {
+       navigation.setOptions({
+        headerRight: () => {
+            return <Button title='Tap me' onPress={headerButtonPressHandler} />
+        }
+       });
+    }, [navigation, headerButtonPressHandler]);
+
     return <ScrollView style={styles.rootContainer}>
         <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
         <Text style={styles.title}>{selectedMeal.title}</Text>
